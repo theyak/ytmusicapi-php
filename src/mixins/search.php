5e6b51efea6763a8532f6adc52d7b816eb550879
theyak <larry@theyak.net>
2026-08-09 19:02:30 -0700

    Search

--- src/mixins/search.php
+++ src/mixins/search.php
@@ -1,9 +1,24 @@
 <?php
 
-//  91, 105, 117..118, 144, 148, 60
-
 namespace Ytmusicapi;
 
+/**
+ * These don't seem to actually do anything :(
+ *
+ * @phpstan-type SearchFilterType
+ *     "songs"|
+ *     "videos"|
+ *     "albums"|
+ *     "artists"|
+ *     "playlists"|
+ *     "community_playlists"|
+ *     "featured_playlists"|
+ *     "profiles"|
+ *     "podcasts"|
+ *     "episodes
+ *
+ * @phpstan-type SearchScopeType "uploads"|"library"
+ */
 trait Search
 {
     /**
@@ -11,10 +26,10 @@ trait Search
      * Returns results within the provided category.
      *
      * @param string $query Query string, e.g., 'Oasis Wonderwall'
-     * @param string|null $filter Filter for item types. Allowed values: `songs`, `videos`, `albums`, `artists`, `playlists`, `community_playlists`, `featured_playlists`, `uploads`.
+     * @param ?SearchFilterType $filter Filter for item types. Allowed values: `songs`, `videos`, `albums`, `artists`, `playlists`, `community_playlists`, `featured_playlists`,  `profiles`, `podcasts`, `episodes`.
      *   This is similar to clicking "View more" on the YouTube Music search results page
      *   Default: Default search, including all types of items.
-     * @param string|null $scope Search scope. Allowed values: `library`, `uploads`.
+     * @param ?SearchScopeType $scope Search scope. Allowed values: `library`, `uploads`.
      *   Default: Search the public YouTube Music catalogue.
      *   Changing scope from the default will reduce the number of settable filters. Setting a filter that is not permitted will throw an exception.
      *   For uploads, no filter can be set.
@@ -101,11 +116,12 @@ trait Search
         }
 
         // set filter for parser
+        $internal_filter = $filter;
         $result_type = null;
-        if ($filter && strpos($filter, "playlists") !== false) {
-            $filter = "playlists";
+        if ($internal_filter && strpos($internal_filter, "playlists") !== false) {
+            $internal_filter = "playlists";
         } elseif ($scope === "uploads") {
-            $filter = "uploads";
+            $internal_filter = "uploads";
             $result_type = "upload";
         }
 
@@ -131,15 +147,30 @@ trait Search
             } elseif (isset($res->musicShelfRenderer)) {
                 $shelf_contents = $res->musicShelfRenderer->contents;
                 $category = nav($res, join(MUSIC_SHELF, TITLE_TEXT), true);
-
-                # if we know the filter it's easy to set the result type
-                # unfortunately uploads is modeled as a filter (historical reasons),
-                # so we take care to not set the result type for that scope
-                if ($filter && $filter !== $scopes[1]) {
-                    $result_type = strtolower(substr($filter, 0, -1));
+            } elseif (isset($res->itemSectionRenderer)) {
+                $shelf_contents = $res->itemSectionRenderer->contents;
+                if (!isset($shelf_contents[0]->musicResponsiveListItemRenderer)) {
+                    continue;
                 }
             } else {
                 continue;
+            }
+
+            # if we know the filter it's easy to set the result type
+            # unfortunately uploads is modeled as a filter (historical reasons),
+            #  so we take care to not set the result type for that scope
+            if (isset($res->musicShelfRenderer) || isset($res->itemSectionRenderer)) {
+                if ($internal_filter && $scope !== "uploads") {
+                    # YTM sometimes pads results with a differently-categorized shelf
+                    # (e.g. a "Songs" shelf when filtering by featured_playlists) - skip
+                    # it rather than mislabeling its contents as the requested type
+                    if ($category) {
+                        $string = strtolower(substr($internal_filter, 0, -1));
+                        if (!str_contains(strtolower($category), $string)) {
+                            continue;
+                        }
+                    }
+                }
             }
 
             $search_results = array_merge(
@@ -147,7 +178,7 @@ trait Search
                 parse_search_results($shelf_contents, $result_type, $category)
             );
 
-            if ($filter) {  // if filter is set, there are continuations
+            if ($internal_filter) {  // if filter is set, there are continuations
                 $request_func = function ($additionalParams) use ($endpoint, $body) {
                     return $this->_send_request($endpoint, $body, $additionalParams);
                 };
@@ -195,7 +226,7 @@ trait Search
 
     /**
      * Remove search suggestion from the user search history.
-     * 
+     *
      * Example usage:
      *   $suggestions = $ytmusic->get_search_suggestions("fade", true);
      *   $success = $ytmusic->remove_search_suggestions($suggestions, [0]);
@@ -209,7 +240,7 @@ trait Search
      *   (with $detailed_runs=true)`
      * @param array|null $indices Optional. The indices of the suggestions to be removed. Default: remove all suggestions.
      * @return bool true if the operation was successful, false otherwise.
-     * 
+     *
      * @throws YTMusicUserError If no search result from history is provided.
      * @throws YTMusicUserError If the index is out of range.
      */
@@ -230,7 +261,7 @@ trait Search
                 "Ensure that you have searched a similar term before."
             );
         }
-        
+
         if (!$indices) {
             $indices = range(0, count($suggestions) -  1);
         }
